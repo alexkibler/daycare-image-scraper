4dc4398050356b62d4448edd8d3808f416b686b7
Alex Kibler <RKibler@ppg.com>
2022-05-02 16:59:24 -0400

    Switched to API-based scraping

--- index.js
+++ index.js
@@ -29,29 +29,49 @@ const HOUR = MINUTE * 60;
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         if (await login(page)) {
-            await page.goto('https://www.tadpoles.com/parents');
-            const links = await page.$$eval('a.fancybox', e=>e.map(a=>{return {id:a.id, href:a.href}}));
-            console.log(`Got ${links.length} images. Downloading now`);
-            for(var i = 0;i < links.length; i++) {
-            if (fs.existsSync(`./images/${links[i].id}.png`)) {
-                console.log(`Image ${i} already exists. Skipping.`);
-            } else {
-                console.log('Downloading image: ' + i);
-                const pageNew = await browser.newPage()
-                const response = await pageNew.goto(links[i].href, {timeout: 0, waitUntil: 'networkidle0'})
-                const imageBuffer = await response.buffer()
-                await fs.promises.writeFile(`./images/${links[i].id}.png`, imageBuffer)
-                pageNew.close();
+            await page.goto('https://www.tadpoles.com/remote/v1/events?direction=range&earliest_event_time=1&latest_event_time=164369160000&num_events=300&client=dashboard&type=Activity');
+            var apiResponse = await page.content();
+
+            const innerText = await page.evaluate(() =>  {
+                return JSON.parse(document.querySelector("body").innerText); 
+            }); 
+            for (var i = 0; i < innerText.events.length; i++) {
+                const obj = innerText.events[i].key;
+                const key = innerText.events[i].attachments[0];
+                var url = `https://www.tadpoles.com/remote/v1/obj_attachment?obj=${obj}&key=${key};`
+                const response = await page.goto(url, {timeout: 0, waitUntil: 'networkidle0'});
+                if (!fs.existsSync(`./images/${innerText.events[i].event_date}/${innerText.events[i].attachments[0]}.png`)) {
+                    console.log('Saving image from ' + innerText.events[i].event_date);
+                    await fs.mkdirSync(`./images/${innerText.events[i].event_date}`, { recursive: true});
+                    const imageBuffer = await response.buffer()
+                    await fs.promises.writeFile(`./images/${innerText.events[i].event_date}/${innerText.events[i].attachments[0]}.png`, imageBuffer)
+                } else {
+                    console.log('Image already exists: ' + innerText.events[i].event_date);
+                }
+                await new Promise(r => setTimeout(r, 100));
             }
-            }
-            console.log('All done!');     
-            await browser.close();
+            // const links = await page.$$eval('a.fancybox', e=>e.map(a=>{return {id:a.id, href:a.href}}));
+            // console.log(`Got ${links.length} images. Downloading now`);
+            // for(var i = 0;i < links.length; i++) {
+            // if (fs.existsSync(`./images/${links[i].id}.png`)) {
+            //     console.log(`Image ${i} already exists. Skipping.`);
+            // } else {
+            //     console.log('Downloading image: ' + i);
+            //     const pageNew = await browser.newPage()
+            //     const response = await pageNew.goto(links[i].href, {timeout: 0, waitUntil: 'networkidle0'})
+            //     const imageBuffer = await response.buffer()
+            //     await fs.promises.writeFile(`./images/${links[i].id}.png`, imageBuffer)
+            //     pageNew.close();
+            // }
+            // }
+            // console.log('All done!');     
+            // await browser.close();
         } else {
             console.log('Login failed');
         }
 
     }
 
-    setInterval(await scrapeBabies, MINUTE*5);
+    setInterval(await scrapeBabies, HOUR);
     await scrapeBabies();
 })();
