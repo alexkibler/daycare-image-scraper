0039f9197b7324803bfd6d41281d7bc51ab9c800
Alex Kibler <RKibler@ppg.com>
2022-05-02 14:25:10 -0400

    Refactored a bit, reduced window to 5 minutes

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const HOUR = MINUTE * 60;
 
 (async () => {
 
-    async function scrapeBabies() {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
+    async function login(page) {
         const cookiesFilePath = 'cookies.json';
         const previousSession = fs.existsSync(cookiesFilePath)
         if (previousSession) {
-          // If file exist load the cookies
           const cookiesString = fs.readFileSync(cookiesFilePath);
           const parsedCookies = JSON.parse(cookiesString);
           if (parsedCookies.length !== 0) {
@@ -21,34 +18,40 @@ const HOUR = MINUTE * 60;
               await page.setCookie(cookie)
             }
             console.log('Session has been loaded in the browser')
+            return true
           }
-        }
-      
-        await page.goto('https://www.tadpoles.com/parents');
-        const links = await page.$$eval('a.fancybox', e=>e.map(a=>{return {id:a.id, href:a.href}}));
-        console.log(`Got ${links.length} images. Downloading now`);
-        //const html = await imageDivsHandle.evaluate((imageDivs) => imageDivs.innerHTML, imageDivsHandle)
-        for(var i = 0;i < links.length; i++) {
-          if (fs.existsSync(`./images/${links[i].id}.png`)) {
-              console.log(`Image ${i} already exists. Skipping.`);
-          } else {
-              console.log('Downloading image: ' + i);
-              const pageNew = await browser.newPage()
-              const response = await pageNew.goto(links[i].href, {timeout: 0, waitUntil: 'networkidle0'})
-              const imageBuffer = await response.buffer()
-              await fs.promises.writeFile(`./images/${links[i].id}.png`, imageBuffer)
-              pageNew.close();
-          }
-        }
-        console.log('All done!');
-       
-      //   for(var i = 0; i < links.length; i++) {
-      //       await page.goto(links[i].href);
-      //       await page.screenshot({path: './images/'+links[i].id+'.png'});
-      //   }
-      
-        await browser.close();
+          return false;
+        } 
+        return false;
     }
 
-    setInterval(await scrapeBabies, HOUR);
+    async function scrapeBabies() {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        if (await login(page)) {
+            await page.goto('https://www.tadpoles.com/parents');
+            const links = await page.$$eval('a.fancybox', e=>e.map(a=>{return {id:a.id, href:a.href}}));
+            console.log(`Got ${links.length} images. Downloading now`);
+            for(var i = 0;i < links.length; i++) {
+            if (fs.existsSync(`./images/${links[i].id}.png`)) {
+                console.log(`Image ${i} already exists. Skipping.`);
+            } else {
+                console.log('Downloading image: ' + i);
+                const pageNew = await browser.newPage()
+                const response = await pageNew.goto(links[i].href, {timeout: 0, waitUntil: 'networkidle0'})
+                const imageBuffer = await response.buffer()
+                await fs.promises.writeFile(`./images/${links[i].id}.png`, imageBuffer)
+                pageNew.close();
+            }
+            }
+            console.log('All done!');     
+            await browser.close();
+        } else {
+            console.log('Login failed');
+        }
+
+    }
+
+    setInterval(await scrapeBabies, MINUTE*5);
+    await scrapeBabies();
 })();
