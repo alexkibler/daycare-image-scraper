fbae5a604eee95ca31949b072cddbc5a86a1f17a
Alex Kibler <RKibler@ppg.com>
2022-05-04 11:40:06 -0400

    refactoring

--- index.js
+++ index.js
@@ -9,6 +9,58 @@ const NUMBER_OF_DAYS_TO_SCRAPE = 2;
 
 (async () => {
 
+
+    async function scrape() {
+        console.log(`Starting to scrape.  Current time is ${new Date().toISOString()}`)
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        if (await login(page)) {
+            let startDate = getStartDate();
+            await page.goto(`${process.env.API_BASE_URL}/events?direction=range&earliest_event_time=${startDate}&latest_event_time=${Math.ceil(Date.now() / 1000)}&num_events=300&client=dashboard&type=Activity`);
+            var apiResponse = await page.content();
+
+            const innerText = await page.evaluate(() =>  {
+                return JSON.parse(document.querySelector("body").innerText); 
+            }); 
+            await processRecords(innerText, page);
+            console.log('All done!');     
+        } else {
+            console.log('Login failed');
+        }
+        await browser.close();
+
+    }
+
+    setInterval(await scrape, HOUR);
+    await scrape();
+
+    function getStartDate() {
+        let startDate = new Date();
+        startDate.setDate(startDate.getDate() - NUMBER_OF_DAYS_TO_SCRAPE);
+        startDate = Math.floor(startDate.getTime() / 1000);
+        return startDate;
+    }
+
+    async function processRecords(innerText, page) {
+        for (var i = 0; i < innerText.events.length; i++) {
+            const obj = innerText.events[i].key;
+            const key = innerText.events[i].attachments[0];
+            var url = `${process.env.API_BASE_URL}/obj_attachment?obj=${obj}&key=${key};`;
+            try {
+                const response = await page.goto(url, { timeout: 0, waitUntil: 'networkidle0' });
+                if (innerText.events[i].new_attachments[0]?.mime_type == 'video/mp4') {
+                    // TODO: figure out why videos don't work
+                } else {
+                    await downloadImage(innerText, i, response);
+                }
+
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
+    }
+
     async function login(page) {
         const cookiesFilePath = 'cookies.json';
         const previousSession = fs.existsSync(cookiesFilePath)
@@ -27,58 +79,16 @@ const NUMBER_OF_DAYS_TO_SCRAPE = 2;
         return false;
     }
 
-    async function scrapeBabies() {
-        console.log(`Starting to scrape.  Current time is ${new Date().toISOString()}`)
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        if (await login(page)) {
-            let startDate = new Date();
-            startDate.setDate(startDate.getDate() - NUMBER_OF_DAYS_TO_SCRAPE);
-            startDate = Math.floor(startDate.getTime() / 1000)
-            await page.goto(`${process.env.API_BASE_URL}/events?direction=range&earliest_event_time=${startDate}&latest_event_time=${Math.ceil(Date.now() / 1000)}&num_events=300&client=dashboard&type=Activity`);
-            var apiResponse = await page.content();
-
-            const innerText = await page.evaluate(() =>  {
-                return JSON.parse(document.querySelector("body").innerText); 
-            }); 
-            for (var i = 0; i < innerText.events.length; i++) {
-                const obj = innerText.events[i].key;
-                const key = innerText.events[i].attachments[0];
-                var url = `${process.env.API_BASE_URL}/obj_attachment?obj=${obj}&key=${key};`
-                try
-                {
-                    const response = await page.goto(url, {timeout: 0, waitUntil: 'networkidle0'});
-                    if (innerText.events[i].new_attachments[0]?.mime_type == 'video/mp4'){
-
-                        // TODO: figure out why videos don't work
-
-                    } else {
-                        const path = `${process.env.DOWNLOAD_PATH}/${innerText.events[i].event_date.substr(0,7)}/${innerText.events[i].event_time}.png`
-                        if (!fs.existsSync(path)) {
-                            console.log('Saving image from ' + innerText.events[i].event_date);
-                            await fs.mkdirSync(`${process.env.DOWNLOAD_PATH}/${innerText.events[i].event_date.substr(0,7)}`, { recursive: true});
-                            const imageBuffer = await response.buffer()
-                            await fs.promises.writeFile(path, imageBuffer)
-                        } else {
-                            console.log('Image already exists: ' + innerText.events[i].event_date);
-                        }
-                        await utimes(path, +(innerText.events[i].event_time.toString()+'000'))
-                    }
-
-                }
-                catch (error)
-                {
-                    console.error(error);
-                }
-            }
-            console.log('All done!');     
+    async function downloadImage(innerText, i, response) {
+        const path = `${process.env.DOWNLOAD_PATH}/${innerText.events[i].event_date.substr(0, 7)}/${innerText.events[i].event_time}.png`;
+        if (!fs.existsSync(path)) {
+            console.log('Saving image from ' + innerText.events[i].event_date);
+            await fs.mkdirSync(`${process.env.DOWNLOAD_PATH}/${innerText.events[i].event_date.substr(0, 7)}`, { recursive: true });
+            const imageBuffer = await response.buffer();
+            await fs.promises.writeFile(path, imageBuffer);
         } else {
-            console.log('Login failed');
+            console.log('Image already exists: ' + innerText.events[i].event_date);
         }
-        await browser.close();
-
+        await utimes(path, +(innerText.events[i].event_time.toString() + '000'));
     }
-
-    setInterval(await scrapeBabies, HOUR);
-    await scrapeBabies();
 })();
